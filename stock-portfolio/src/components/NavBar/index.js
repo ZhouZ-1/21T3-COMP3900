07ee31ac69c0ac0e3e9457da74adee1f1265ef8b
Tony Jang <eui.chang@student.unsw.edu.au>
2021-10-10 16:08:50 +1100

    clicking home button now goes to the main page

--- stock-portfolio/src/components/NavBar/index.js
+++ stock-portfolio/src/components/NavBar/index.js
@@ -22,7 +22,7 @@ function NavBar(){
         // navbar navbar-dark bg-primary
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Home</a>
+                <a class="navbar-brand" onClick={() => history.push('/')}>Home</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
--- stock-portfolio/src/components/SignUp/helper.js
+++ stock-portfolio/src/components/SignUp/helper.js
@@ -1,22 +1,17 @@
 export function validatePassword(p) {
-    var errors = [];
     if (p.length < 8) {
-        // errors.push("Your password must be at least 8 characters"); 
         return false;
     }
     if (p.search(/[a-z]/i) < 0) {
         return false;
-        // errors.push("Your password must contain at least one letter.");
     }
     if (p.search(/[0-9]/) < 0) {
         return false;
-        // errors.push("Your password must contain at least one digit."); 
     }
     return true;
 }
 
 export function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    // console.log();
     return re.test(email);
 }
