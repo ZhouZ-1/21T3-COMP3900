6f4c692361f53b826dd2cc6ce71b6fdfcfff17ad
jjamme <fnguyen2000@hotmail.com>
2021-10-12 20:26:43 +1100

    Add history push

--- stock-portfolio/src/components/AccountPage/index.js
+++ stock-portfolio/src/components/AccountPage/index.js
@@ -13,52 +13,30 @@ import BasicTabs from './../AccNav/index';
 // import UpdatePassword from "../Updatepwd/index";
 import api from '../../api';
 
-const handleDelete = async () => {
-  const confirmDelete = window.confirm(
-    'Are you sure you want to delete your account?'
-  );
-
-  if (confirmDelete) {
-    api('accounts/delete', 'DELETE', {
-      token: localStorage.getItem('token'),
-    }).then((resp) => {
-      if (resp.is_success) {
-        localStorage.removeItem('token');
-      }
-    });
-  }
-};
-
 function AccountPage() {
   var history = useHistory();
+
+  const handleDelete = async () => {
+    const confirmDelete = window.confirm(
+      'Are you sure you want to delete your account?'
+    );
+
+    if (confirmDelete) {
+      api('accounts/delete', 'DELETE', {
+        token: localStorage.getItem('token'),
+      }).then((resp) => {
+        if (resp.is_success) {
+          localStorage.removeItem('token');
+          history.push('/');
+        }
+      });
+    }
+  };
 
   return (
     <>
       <BasicTabs />
-      {/* <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to='/account'>Account Details</Link>
-            </li>
-            <li>
-              <Link to='/updatepwd'>Update Account</Link>
-            </li>
-          </ul>
-
-          <hr />
-
-          <Switch>
-            <Route exact path='/account'>
-              <AccDetails />
-            </Route>
-            <Route path='/updatepwd'>
-              <UpdatePassword />
-            </Route>
-          </Switch>
-        </div>
-      </Router> */}
-      <Button className='delete-button' onClick={handleDelete}>
+      <Button className='delete-button' onClick={(history) => handleDelete()}>
         Delete
       </Button>
     </>
