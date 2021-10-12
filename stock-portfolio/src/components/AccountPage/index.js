e01b7e822a3131c3f63383b3ddfc4dd60f25d623
AM-lam <lamanning726@gmail.com>
2021-10-13 03:44:25 +1100

    redirect in the page page tbc

--- stock-portfolio/src/components/AccountPage/index.js
+++ stock-portfolio/src/components/AccountPage/index.js
@@ -1,41 +1,13 @@
-// import {
-//     BrowserRouter as Router,
-//     Route,
-//     Switch,
-//     Link,
-//   } from "react-router-dom";
-import { Button } from '@mui/material';
 import React from 'react';
 import { useHistory } from 'react-router';
-import BasicGrid from './../AccNav/index';
+import AccNav from './../AccNav/index';
 import api from '../../api';
 
 function AccountPage() {
-  var history = useHistory();
-
-  const handleDelete = async () => {
-    const confirmDelete = window.confirm(
-      'Are you sure you want to delete your account?'
-    );
-
-    if (confirmDelete) {
-      api('accounts/delete', 'DELETE', {
-        token: localStorage.getItem('token'),
-      }).then((resp) => {
-        if (resp.is_success) {
-          localStorage.removeItem('token');
-          history.push('/');
-        }
-      });
-    }
-  };
-
   return (
     <>
-      <BasicGrid />
-      <Button className='delete-button' onClick={(history) => handleDelete()}>
-        Delete
-      </Button>
+      <AccNav />
+
     </>
   );
 }
