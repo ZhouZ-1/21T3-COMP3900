e6ac16fab877384a922a539c87f6b04a7a90c88b
Tony Jang <eui.chang@student.unsw.edu.au>
2021-10-16 22:19:39 +1100

    edited stock page to look more nice

--- stock-portfolio/src/components/StockDetails/index.js
+++ stock-portfolio/src/components/StockDetails/index.js
@@ -6,31 +6,76 @@ import NavBar from "../NavBar";
 function StockDetails(){
     let { symbol } = useParams();
     const [stockDetails,setStockDetails] = useState();
-    console.log(symbol);
     useEffect(async ()=>{
         const response = await api('stocks/search', 'POST', {symbol: symbol});
         setStockDetails(response);
     },[symbol]);
     return (
         <>
-        <NavBar></NavBar>
-        <div>
+            <NavBar></NavBar>
             {console.log(stockDetails)}
-            {stockDetails ? 
-                (
-                    <div>
-                        <ul>
-                            <li>stock symbol: {stockDetails.symbol}</li>
-                            <li>stock name: {stockDetails.name}</li>
-                            <li>stock price: {stockDetails.price}</li>
-                            <li>stock sector: {stockDetails.sector}</li>
-                            <li>stock asset type: {stockDetails.asset_type}</li>
-                        </ul>
+            <div class='main-container m-5'>
+                <div>
+                    <div class="d-inline-block stock-description">
+                        <div class='stock-title'>
+                            {stockDetails.name} ({stockDetails.symbol})
+                        </div>
+                        <div class='stock-industry'>
+                            {stockDetails.industry}
+                        </div>
                     </div>
-                ):
-                (<div>retrieving data</div>)
-            }
-        </div>
+                    <div class="d-inline-block watch-list-container">
+                        <div class="watch-list">
+                            <button type="button" class="btn btn-primary watch-list">add to watch list</button>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="stock-price">
+                    {stockDetails.price}
+                </div>
+            </div>
+
+            <div class='graph-container m-5'>
+                <div class="stock-trend-graph">
+                    {/* TODO: TONY - implement graph here */}
+                    <p>graph comes here</p>
+                    <button type="button" class="btn btn-primary 3-month">3M</button>
+                    <button type="button" class="btn btn-primary 6-month">6M</button>
+                    <button type="button" class="btn btn-primary 1-year">1YR</button>
+                </div>
+            </div>
+
+            <div class='stock-statistics-container m-5'>
+                {stockDetails ? 
+                    (
+                        <div class="stock-statistics">
+                            <div class="row justify-content-start">
+                                <div class="col-3">
+                                    52 week high: {stockDetails.year_high}
+                                </div>
+                                <div class="col-3">
+                                    52 week low: {stockDetails.year_low}
+                                </div>
+                            </div>
+                            <div class="row justify-content-start">
+                                <div class="col-3">
+                                    stock price: {stockDetails.price}
+                                </div>
+                                <div class="col-3">
+                                    stock sector: {stockDetails.sector}
+                                </div>
+                            </div>
+                            <div class="row justify-content-start">
+                                <div class="col-3">
+                                stock asset type: {stockDetails.asset_type}
+                                </div>
+                            </div>
+                        </div>
+                    ):
+                    (<div>retrieving data</div>)
+                }
+            </div>
         </>
     );
 }
