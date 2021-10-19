882a569accbce7f3d4fed1abf4138b0dca3424f5
Tony Jang <eui.chang@student.unsw.edu.au>
2021-10-19 19:29:39 +1100

    edit stock list

--- stock-portfolio/src/components/StockList/StockInfoBlock.js
+++ stock-portfolio/src/components/StockList/StockInfoBlock.js
@@ -1,12 +1,27 @@
+import { fontSize, fontWeight } from "@mui/system";
+import api from "../../api";
+
 function StockInfoBlock(props) {
     const {data} = props;
-    
+    const symbol = data[0]
+    const name = data[1]
+    const exchange = data[2]
+    const asset_type = data[3]
+
     return (
         <>
             <div class='mt-2'>
-                <span>symbol: {data[0]}</span>
-                <br></br>
-                <span>name: {data[1]}</span>
+                <div>
+                    <div class='d-inline-block'>
+                        <h5>{name} ({symbol})</h5>
+                    </div>
+                    <div class='d-inline-block ms-3'>
+                        <small class="text-muted">asset type: {asset_type}</small>
+                    </div>
+                </div>
+                <div>
+                    traded in: {exchange}
+                </div>
             </div>
         </>
     );
--- stock-portfolio/src/components/StockList/index.js
+++ stock-portfolio/src/components/StockList/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import api from "../../api";
+import NavBar from "../NavBar";
 import StockInfoBlock from "./StockInfoBlock";
 function StockList() {
     
@@ -7,15 +8,16 @@ function StockList() {
     
     useEffect(async () => {
         const response = await api('stocks/searchall', 'POST');
+        console.log(response);
         const stockData = []
         for (const [symbol, stockDetails] of Object.entries(response.body)) {
-            stockData.push([symbol,stockDetails.name]);
+            stockData.push([symbol,stockDetails.name,stockDetails.exchange,stockDetails.asset_type]);
         }
         const items = stockData.map(function(item){
             return <StockInfoBlock data={item}></StockInfoBlock>
         });
         setStocks(items);
-    });
+    },[]);
 
     // const getStockLists = async (type) => {
     //     console.log('here');
@@ -36,40 +38,45 @@ function StockList() {
     //     });
     //     setStocks(items);
     // }
-    const onPrevPage = () => {
-        const response = await api('stocks/searchprev', 'POST');
-        const stockData = [];
-        for (const [symbol, stockDetails] of Object.entries(response.body)) {
-            stockData.push([symbol,stockDetails.name]);
-        }
-        const items = stockData.map(function(item){
-            return <StockInfoBlock data={item}></StockInfoBlock>
-        });
-        setStocks(items);
-    }
-
-    const onNextPage = () => {
-        const response = await api('stocks/searchnext', 'POST');
-        const stockData = [];
-        for (const [symbol, stockDetails] of Object.entries(response.body)) {
-            stockData.push([symbol,stockDetails.name]);
-        }
-        const items = stockData.map(function(item){
-            return <StockInfoBlock data={item}></StockInfoBlock>
-        });
-        setStocks(items);
-    }
+    const onPrevPage = () => {}
+    // const onPrevPage = () => {
+    //     const response = await api('stocks/searchprev', 'POST');
+    //     const stockData = [];
+    //     for (const [symbol, stockDetails] of Object.entries(response.body)) {
+    //         stockData.push([symbol,stockDetails.name]);
+    //     }
+    //     const items = stockData.map(function(item){
+    //         return <StockInfoBlock data={item}></StockInfoBlock>
+    //     });
+    //     setStocks(items);
+    // }
+    const onNextPage = () => {}
+    // const onNextPage = () => {
+    //     const response = await api('stocks/searchnext', 'POST');
+    //     const stockData = [];
+    //     for (const [symbol, stockDetails] of Object.entries(response.body)) {
+    //         stockData.push([symbol,stockDetails.name]);
+    //     }
+    //     const items = stockData.map(function(item){
+    //         return <StockInfoBlock data={item}></StockInfoBlock>
+    //     });
+    //     setStocks(items);
+    // }
 
     return (
         <>
-            <div>
-                {stocks ? 
-                    (stocks) : 
-                    (<p>loading</p>)
-                }
+            <NavBar></NavBar>
+            <div class="m-5">
+                <div>
+                    {stocks ? 
+                        (stocks) : 
+                        (<p>loading</p>)
+                    }
+                </div>
             </div>
-            <button type="button" class="btn btn-outline-primary" onClick={onPrevPage}>prev</button>
-            <button type="button" class="btn btn-outline-primary" onClick={onNextPage}>next</button>
+            <button type="button" class="btn btn-outline-primary ms-5" onClick={onPrevPage}>prev</button>
+            <button type="button" class="btn btn-outline-primary ms-2" onClick={onNextPage}>next</button>
+            
         </>
     );
 }
