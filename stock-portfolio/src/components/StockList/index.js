967cb562b165eda98320d9f81c1cdd7e9a90fbe7
Tony Jang <eui.chang@student.unsw.edu.au>
2021-10-20 18:18:02 +1100

    complete stock list

--- stock-portfolio/src/components/StockList/index.js
+++ stock-portfolio/src/components/StockList/index.js
@@ -7,61 +7,27 @@ function StockList() {
     const [stocks,setStocks] = useState();
     
     useEffect(async () => {
-        const response = await api('stocks/searchall', 'POST');
-        console.log(response);
-        const stockData = []
+        await getStockList('normal');
+    },[]);
+
+    const getStockList = async (type) => {
+        var response;
+        if (type==='next'){
+            response = await api('stocks/searchnext', 'POST');
+        }else if(type==='prev'){
+            response = await api('stocks/searchprev', 'POST');
+        }else{
+            response = await api('stocks/searchall', 'POST');
+        }
+        const stockData = [];
         for (const [symbol, stockDetails] of Object.entries(response.body)) {
-            stockData.push([symbol,stockDetails.name,stockDetails.exchange,stockDetails.asset_type]);
+            stockData.push([symbol,stockDetails.name]);
         }
         const items = stockData.map(function(item){
             return <StockInfoBlock data={item}></StockInfoBlock>
         });
         setStocks(items);
-    },[]);
-
-    // const getStockLists = async (type) => {
-    //     console.log('here');
-    //     var response;
-    //     if (type === 'next') {
-    //         response = await api('stocks/searchnext', 'POST');
-    //     }else if(type === 'prev'){
-    //         response = await api('stocks/searchprev', 'POST');
-    //     }else{
-    //         response = await api('stocks/searchall', 'POST');
-    //     }
-    //     const stockData = [];
-    //     for (const [symbol, stockDetails] of Object.entries(response.body)) {
-    //         stockData.push([symbol,stockDetails.name]);
-    //     }
-    //     const items = stockData.map(function(item){
-    //         return <StockInfoBlock data={item}></StockInfoBlock>
-    //     });
-    //     setStocks(items);
-    // }
-    const onPrevPage = () => {}
-    // const onPrevPage = () => {
-    //     const response = await api('stocks/searchprev', 'POST');
-    //     const stockData = [];
-    //     for (const [symbol, stockDetails] of Object.entries(response.body)) {
-    //         stockData.push([symbol,stockDetails.name]);
-    //     }
-    //     const items = stockData.map(function(item){
-    //         return <StockInfoBlock data={item}></StockInfoBlock>
-    //     });
-    //     setStocks(items);
-    // }
-    const onNextPage = () => {}
-    // const onNextPage = () => {
-    //     const response = await api('stocks/searchnext', 'POST');
-    //     const stockData = [];
-    //     for (const [symbol, stockDetails] of Object.entries(response.body)) {
-    //         stockData.push([symbol,stockDetails.name]);
-    //     }
-    //     const items = stockData.map(function(item){
-    //         return <StockInfoBlock data={item}></StockInfoBlock>
-    //     });
-    //     setStocks(items);
-    // }
+    }
 
     return (
         <>
@@ -74,8 +40,8 @@ function StockList() {
                     }
                 </div>
             </div>
-            <button type="button" class="btn btn-outline-primary ms-5" onClick={onPrevPage}>prev</button>
-            <button type="button" class="btn btn-outline-primary ms-2" onClick={onNextPage}>next</button>
+            <button type="button" class="btn btn-outline-primary ms-5" onClick={()=>getStockList('prev')}>prev</button>
+            <button type="button" class="btn btn-outline-primary ms-2" onClick={()=>getStockList('next')}>next</button>
             
         </>
     );
