9965a06d66802f833297f4d6917d42200dbea141
jjamme <fnguyen2000@hotmail.com>
2021-10-20 21:43:47 +1100

    Update comment

--- stock-portfolio-backend/namespaces/watchlist.py
+++ stock-portfolio-backend/namespaces/watchlist.py
@@ -80,7 +80,7 @@ class WatchlistAddStocks(Resource):
         if user["watchlist"] == None or user["watchlist"] == "null":
             user["watchlist"] = '[]'
 
-        # add more fields depending on what information we want
+        # turn string into array
         user["watchlist"] = literal_eval(user["watchlist"])
 
         # check if stock is already in watchlist
@@ -91,5 +91,51 @@ class WatchlistAddStocks(Resource):
         db.update_user_by_value(username, "watchlist", json.dumps(user["watchlist"]))
 
         return { 
-            "watchlist": user["watchlist"]
+            "is_success": True,
+        }
+
+@watchlist.route('/remove', doc={
+    "description": "Allows user to remove a stock from their watchlist"
+})
+class WatchlistRemoveStocks(Resource):
+    @watchlist.expect(watchlist_stock_model)
+    @watchlist.response(200, 'Success', watchlist_info_model)
+    @watchlist.response(409, 'Unable to process request')
+    def post(self):
+        """
+        Function that takes a stock returns the overview details of a particular stock
+        """
+        body = request.json
+        username = body['username']
+        stock = body['stock']
+
+        # validity checks
+        # make sure it is a string
+        if not isinstance(username, str):
+            abort(409, f"{username} is not a string")
+
+        user = db.get_user_by_value('username', username)
+
+        if not user:
+            abort(410, "Could not find user")
+
+        if "watchlist" not in user.keys():
+            user["watchlist"] = []
+            db.update_user_by_value(username, "watchlist", [])
+
+        if user["watchlist"] == None or user["watchlist"] == "null":
+            user["watchlist"] = '[]'
+
+        # turn string into array
+        user["watchlist"] = literal_eval(user["watchlist"])
+
+        # check if stock is already in watchlist
+        if stock not in user["watchlist"]:
+            abort(409, f"{stock} is not in your watchlist")
+
+        user["watchlist"].remove(stock)
+        db.update_user_by_value(username, "watchlist", json.dumps(user["watchlist"]))
+
+        return {
+            "is_success": True
         }
