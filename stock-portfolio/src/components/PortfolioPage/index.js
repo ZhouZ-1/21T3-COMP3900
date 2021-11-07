84d0a93ee88696c3f5b04767764f1cbf1bd52f0f
jjamme <fnguyen2000@hotmail.com>
2021-11-07 15:27:56 +1100

    Fix style

--- stock-portfolio/src/components/PortfolioPage/index.js
+++ stock-portfolio/src/components/PortfolioPage/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { useState, useEffect } from "react";
-import { BrowserRouter as Router, 
-  Switch, 
-  Route, 
-  Link } from "react-router-dom"
-import { useHistory } from "react-router";
-import { 
-    Button,  
-    TextField,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { useHistory } from 'react-router';
+import {
+  Button,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
 } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
-import api from "../../api";
-import moment from "moment";
-import Loader from "../Loader";
-import BalancePortfolio from "../BalancePortfolio";
+import api from '../../api';
+import moment from 'moment';
+import Loader from '../Loader';
+import BalancePortfolio from '../BalancePortfolio';
 
 const columns = [
   { field: 'holding_id', headerName: 'id', width: 100 },
@@ -26,8 +23,8 @@ const columns = [
   { field: 'value', headerName: 'Value', width: 120 },
   { field: 'qty', headerName: 'Quantity', width: 130 },
   { field: 'date', headerName: 'Date', width: 130 },
-  { field: 'change_val', headerName: 'Changes', width: 150 }, 
-  { field: 'change_percent', headerName: 'Percentage', width: 150 }
+  { field: 'change_val', headerName: 'Changes', width: 150 },
+  { field: 'change_percent', headerName: 'Percentage', width: 150 },
 ];
 
 function PortfolioPage() {
@@ -35,39 +32,41 @@ function PortfolioPage() {
   const [openDelete, setOpenDelete] = useState(false);
   const [openAdd, setOpenAdd] = useState(false);
   const [openDS, setOpenDS] = useState(false);
-  const [symbol,setSymbol] = useState('');
-  const [qty,setQty] = useState(0);
+  const [symbol, setSymbol] = useState('');
+  const [qty, setQty] = useState(0);
   const [stocks, setStocks] = useState([]);
   const [select, setSelect] = useState([]);
-  const [isLoading,setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [refresh, setRefresh] = useState(0);
   const [overall, setOverall] = useState([]);
 
-  useEffect(async() => {
+  useEffect(async () => {
     setIsLoading(true);
     let newData = [];
 
-    const data = await  api(`invested_performance/portfolio?portfolio=${localStorage.getItem('id')}`, 'GET');
+    const data = await api(
+      `invested_performance/portfolio?portfolio=${localStorage.getItem('id')}`,
+      'GET'
+    );
     await api('portfolio/holdings', 'POST', {
-      token: localStorage.getItem('token'), portfolio_id: localStorage.getItem('id')
-    })
-      .then(res => {
-        setOverall(data.symbols.filter(c => { 
-          if (c.symbol != 'overall') 
-          return c;
-        }));
-        const overall = data.symbols.filter(c => { 
-          if (c.symbol == 'overall') 
-          return c;
-        });
+      token: localStorage.getItem('token'),
+      portfolio_id: localStorage.getItem('id'),
+    }).then((res) => {
+      setOverall(
+        data.symbols.filter((c) => {
+          if (c.symbol != 'overall') return c;
+        })
+      );
+      const overall = data.symbols.filter((c) => {
+        if (c.symbol == 'overall') return c;
+      });
 
-        res.map(s => {
-          const changes = data.symbols.filter(c => { 
-            if (c.symbol == s.symbol) 
-            return c;
-          })[0]; 
-          newData.push({id: s.holding_id, ...s,...changes});
-        });
+      res.map((s) => {
+        const changes = data.symbols.filter((c) => {
+          if (c.symbol == s.symbol) return c;
+        })[0];
+        newData.push({ id: s.holding_id, ...s, ...changes });
+      });
     });
 
     setStocks(newData);
@@ -100,95 +99,99 @@ function PortfolioPage() {
 
   const getCurrDate = () => {
     let curr = new Date();
-    let date = curr.getDate() + '/' + (curr.getMonth()+1) + '/' + curr.getFullYear();
+    let date =
+      curr.getDate() + '/' + (curr.getMonth() + 1) + '/' + curr.getFullYear();
     return date;
   };
-  
-  const searchStock = async(s) => {
+
+  const searchStock = async (s) => {
     let value = -1;
-    const res = await api(`stocks/search`, 'POST', {symbol: s}); 
+    const res = await api(`stocks/search`, 'POST', { symbol: s });
     if (res.price) {
       return res.price;
-    } 
+    }
     return value;
   };
-  
+
   const addStock = async () => {
     setIsLoading(true);
     const date = getCurrDate();
-    const value = await searchStock(symbol); 
+    const value = await searchStock(symbol);
     // let value = await searchStock(symbol);
 
     if (!(symbol && qty)) {
-      alert("Missing Symbol/Quantity field.");
+      alert('Missing Symbol/Quantity field.');
       handleCloseAdd();
       return;
     }
 
     if (!value) {
-      alert("Stock Symbol not exist.");
+      alert('Stock Symbol not exist.');
       handleCloseAdd();
       return;
-    } 
+    }
 
     if (qty < 1) {
-      alert("Quantity cannot be less than 1.");
+      alert('Quantity cannot be less than 1.');
       return;
     }
 
     const res = await api('portfolio/holdings/add', 'POST', {
-      token: localStorage.getItem('token'), 
+      token: localStorage.getItem('token'),
       portfolio_id: localStorage.getItem('id'),
       symbol: symbol.toUpperCase(),
       value: value,
       qty: qty,
-      type: "buy",
-      brokerage: "9.95",
-      exchange: "NYSE",
+      type: 'buy',
+      brokerage: '9.95',
+      exchange: 'NYSE',
       date: date,
-      currency: "USD"
+      currency: 'USD',
     });
 
     if (res.is_success) {
-      alert("Successfully Add Stock!");
-      setRefresh(r => r +1);
-    } 
-    
+      alert('Successfully Add Stock!');
+      setRefresh((r) => r + 1);
+    }
+
     handleCloseAdd();
     setIsLoading(false);
   };
-  
+
   const deleteStock = async () => {
     setIsLoading(true);
 
     if (select.length == 0) {
-      alert("You have not select any stocks.");
+      alert('You have not select any stocks.');
       return;
     }
 
-    Promise.all(select.map((id) => {
-      const res = api('portfolio/holdings/delete', 'DELETE', {
-        token: localStorage.getItem('token'), 
-        holding_id: id
+    Promise.all(
+      select.map((id) => {
+        const res = api('portfolio/holdings/delete', 'DELETE', {
+          token: localStorage.getItem('token'),
+          holding_id: id,
+        });
       })
-    }))
-      .then(res => {
-        if (res !== undefined) {
-          alert("Successfully Delete Stock(s)!");
-          setRefresh(r => r +1);
-          setIsLoading(false);
-        }});
+    ).then((res) => {
+      if (res !== undefined) {
+        alert('Successfully Delete Stock(s)!');
+        setRefresh((r) => r + 1);
+        setIsLoading(false);
+      }
+    });
     handleCloseDS();
   };
 
   const handleDelete = async () => {
     setIsLoading(true);
     const res = await api('portfolio/delete', 'DELETE', {
-      token: localStorage.getItem('token'), portfolio_id: localStorage.getItem('id')
+      token: localStorage.getItem('token'),
+      portfolio_id: localStorage.getItem('id'),
     });
 
     if (res) {
-      alert("Successfully Delete The Portfolio.");
+      alert('Successfully Delete The Portfolio.');
       localStorage.removeItem('id');
       history.push('/viewPortfolio');
     }
@@ -196,83 +199,93 @@ function PortfolioPage() {
     handleCloseDelete();
   };
 
-  // const handleOverview = () => {
-  //   return (
-  //     <Link to={{ 
-  //       pathname: "/portfolioBalance", 
-  //       state: { overall: overall }
-  //      }}>
-  //       Portfolio Balance
-  //      </Link>
-  //   );
-  // };
-
-
   return (
     <div>
       <div>
-        <h1>Portfolio: {localStorage.getItem('name')}
-        { !isLoading && 
-          // <button class='btn btn-lg btn-link btn-block' onClick={handleOverview}>Portfolio Balance</button>
-          <Link to={{
-            pathname: "/portfolioBalance",
-            state: { detail: overall }
-          }}>Portfolio Balance</Link>
-
-        }
+        <h1>
+          Portfolio: {localStorage.getItem('name')}
+          {!isLoading && (
+            // <button class='btn btn-lg btn-link btn-block' onClick={handleOverview}>Portfolio Balance</button>
+            <Link
+              to={{
+                pathname: '/portfolioBalance',
+                state: { detail: overall },
+              }}
+            >
+              Portfolio Balance
+            </Link>
+          )}
         </h1>
         <br></br>
         <div>
-          <Button class="btn btn-outline-primary ms-5" onClick={handleClickOpenAdd}>Add Stock</Button>
-          <Dialog
-              open={openAdd}
-              onClose={handleCloseAdd}
-              aria-labelledby="alert-dialog-title"
-              aria-describedby="alert-dialog-description"
+          <Button
+            class='btn btn-outline-primary ms-5'
+            onClick={handleClickOpenAdd}
           >
-              <DialogTitle id="alert-dialog-title">
-              {"Add Stock"}
-              </DialogTitle>
-              <DialogContent>
-              <DialogContentText id="alert-dialog-description">
-                  Please Enter the Symbol of Stock:
+            Add Stock
+          </Button>
+          <Dialog
+            open={openAdd}
+            onClose={handleCloseAdd}
+            aria-labelledby='alert-dialog-title'
+            aria-describedby='alert-dialog-description'
+          >
+            <DialogTitle id='alert-dialog-title'>{'Add Stock'}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id='alert-dialog-description'>
+                Please Enter the Symbol of Stock:
               </DialogContentText>
-              <TextField id="demo-helper-text-misaligned-no-helper" label="symbol" required onChange={(evt)=>setSymbol(evt.target.value)}></TextField>
-              <TextField id="demo-helper-text-misaligned-no-helper" label="Quantity" required onChange={(evt)=>setQty(evt.target.value)}></TextField>
-              </DialogContent>
-              <DialogActions>
+              <TextField
+                id='demo-helper-text-misaligned-no-helper'
+                label='symbol'
+                required
+                onChange={(evt) => setSymbol(evt.target.value)}
+              ></TextField>
+              <TextField
+                id='demo-helper-text-misaligned-no-helper'
+                label='Quantity'
+                required
+                onChange={(evt) => setQty(evt.target.value)}
+              ></TextField>
+            </DialogContent>
+            <DialogActions>
               <Button onClick={handleCloseAdd}>Cancel</Button>
-              <Button onClick={addStock} autoFocus>Confirm</Button>
-              </DialogActions>
+              <Button onClick={addStock} autoFocus>
+                Confirm
+              </Button>
+            </DialogActions>
           </Dialog>
-          <Button class="btn btn-outline-primary ms-5" onClick={handleClickOpenDS}>Delete Stock</Button>
-          <Dialog
-              open={openDS}
-              onClose={handleCloseDS}
-              aria-labelledby="alert-dialog-title"
-              aria-describedby="alert-dialog-description"
+          <Button
+            class='btn btn-outline-primary ms-5'
+            onClick={handleClickOpenDS}
           >
-              <DialogTitle id="alert-dialog-title">
-              {"Delete Stock"}
-              </DialogTitle>
-              <DialogContent>
-              <DialogContentText id="alert-dialog-description">
+            Delete Stock
+          </Button>
+          <Dialog
+            open={openDS}
+            onClose={handleCloseDS}
+            aria-labelledby='alert-dialog-title'
+            aria-describedby='alert-dialog-description'
+          >
+            <DialogTitle id='alert-dialog-title'>{'Delete Stock'}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id='alert-dialog-description'>
                 Do You Want To Delete These Stock(s)?
               </DialogContentText>
-              </DialogContent>
-              <DialogActions>
+            </DialogContent>
+            <DialogActions>
               <Button onClick={handleCloseDS}>Cancel</Button>
-              <Button onClick={deleteStock} autoFocus>Confirm</Button>
-              </DialogActions>
+              <Button onClick={deleteStock} autoFocus>
+                Confirm
+              </Button>
+            </DialogActions>
           </Dialog>
         </div>
         <br></br>
       </div>
       <div style={{ height: 400, width: '100%' }}>
-        { isLoading &&
-            (<Loader></Loader>)
-            }
-        { !isLoading &&
+        {isLoading && <Loader></Loader>}
+        {!isLoading && (
           <DataGrid
             rows={stocks}
             columns={columns}
@@ -286,30 +299,30 @@ function PortfolioPage() {
             }}
             selectionModel={select}
           />
-        }
+        )}
       </div>
       <div>
-        <Button onClick={handleClickOpenDelete}>
-          Delete Portfolio
-        </Button>
+        <Button onClick={handleClickOpenDelete}>Delete Portfolio</Button>
         <Dialog
           open={openDelete}
           onClose={handleCloseDelete}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
+          aria-labelledby='alert-dialog-title'
+          aria-describedby='alert-dialog-description'
         >
-          <DialogTitle id="alert-dialog-title">
-              {"Delete Portfolio"}
-            </DialogTitle>
-            <DialogContent>
-              <DialogContentText id="alert-dialog-description">
-                Do You Want To Delete This Portfolio?
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleCloseDelete}>Cancel</Button>
-              <Button onClick={handleDelete} autoFocus>Confirm</Button>
-            </DialogActions>
+          <DialogTitle id='alert-dialog-title'>
+            {'Delete Portfolio'}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id='alert-dialog-description'>
+              Do You Want To Delete This Portfolio?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseDelete}>Cancel</Button>
+            <Button onClick={handleDelete} autoFocus>
+              Confirm
+            </Button>
+          </DialogActions>
         </Dialog>
       </div>
     </div>
@@ -317,4 +330,3 @@ function PortfolioPage() {
 }
 
 export default PortfolioPage;
-
