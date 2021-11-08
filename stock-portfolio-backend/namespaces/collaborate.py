ef7c700224dfe5a2e38fd89ce17d8ba5368414b0
Jun <z5062476@ad.unsw.edu.au>
2021-11-09 01:33:33 +1100

    Basic add, edit and remove done. needs testing

--- stock-portfolio-backend/namespaces/collaborate.py
+++ stock-portfolio-backend/namespaces/collaborate.py
@@ -85,6 +85,7 @@ class Reply(Resource):
             "is_success": True
         }
 
+# might not need new routes for holdings 
 @collaborate.route('/add-holding', doc={
     'description': 'Adds a holding to a collabortive portfolio',
 })
@@ -92,6 +93,39 @@ class Add(Resource):
     @collaborate.expect(add_stock_model)
     @collaborate.response(200, 'Success', success_model)
     def post(self):
+
+        # check if below can be done by just redirecting
+        """
+        Add a holding to the portfolio.
+        """
+        body = request.json
+        portfolio_id = body['portfolio_id']
+        symbol = body['symbol']
+        value = body['value']
+        qty = body['qty']
+        type = body['type']
+        brokerage = body['brokerage']
+        exchange = body['exchange']
+        date = body['date']
+        currency = body['currency']
+        token = body['token']
+
+        # Get username from token
+        user = db.get_user_by_value("active_token", token)
+        if not user:
+            abort(400, "Token is invalid") 
+
+        # Check that user owns or has edit permissions for the portfolio
+        portfolio = db.query_portfolio(portfolio_id)
+        if portfolio is None or portfolio["owner"] != user["username"] or query_check_edit_permissions(portfolio_id, user['username']):
+            abort(400, "User does not have permission to add stocks to the portfolio")
+
+        # Check that the type is either buy or sell.
+        if type not in ['buy', 'sell']:
+            abort(400, "Type is invalid")
+
+        # Add stock to portfolio.
+        db.add_stock(portfolio_id, symbol, value, qty, type, brokerage, exchange, date, currency)
         return {
             "is_success": True
         }
@@ -103,6 +137,31 @@ class Edit(Resource):
     @collaborate.expect(edit_stock_model)
     @collaborate.response(200, 'Success', success_model)
     def put(self):
+        """
+        Edit holding details.
+        """
+        body = request.json
+        token = body['token']
+        holding_id = body['holding_id']
+        
+        holding_details = body
+        holding_details.pop('token')
+        holding_details.pop('holding_id')
+
+        # Get username from token
+        user = db.get_user_by_value("active_token", token)
+        if not user:
+            abort(400, "Token is invalid")
+        
+        # Check that user owns or has edit permissions for the portfolio
+        portfolio_id = db.get_portfolio_id_from_holding(holding_id)
+        portfolio = db.query_portfolio(portfolio_id)
+        if portfolio is None or portfolio["owner"] != user["username"] or query_check_edit_permissions(portfolio_id, user['username']):
+            abort(400, "User cannot access this holding.")
+
+        # Update holding details.
+        db.update_holding(holding_id, holding_details)
+
         return {
             "is_success": True
         }
@@ -114,6 +173,27 @@ class Remove(Resource):
     @collaborate.expect(delete_holding_model)
     @collaborate.response(200, 'Success', success_model)
     def delete(self):
+        """
+        Deletes a holding from the portfolio.
+        """
+        body = request.json
+        holding_id = body['holding_id']
+        token = body['token']
+
+        # Get username from token
+        user = db.get_user_by_value("active_token", token)
+        if not user:
+            abort(400, "Token is invalid")
+
+         # Check that user owns or has edit permissions for the portfolio
+        portfolio_id = db.get_portfolio_id_from_holding(holding_id)
+        portfolio = db.query_portfolio(portfolio_id)
+        if portfolio is None or portfolio["owner"] != user["username"] or query_check_edit_permissions(portfolio_id, user['username']):
+            abort(400, "User cannot access this holding.")
+
+        # Delete the holding.
+        db.remove_holding(holding_id)
+
         return {
             "is_success": True
         }
