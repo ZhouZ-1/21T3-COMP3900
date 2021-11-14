111f9c0e5930ad04f3140e5c05cf9933d8fe2c69
Tony Jang <eui.chang@student.unsw.edu.au>
2021-11-14 20:36:34 +1100

    fixing a bug in collab portfolio

--- stock-portfolio/src/components/CollabList/index.js
+++ stock-portfolio/src/components/CollabList/index.js
@@ -1,46 +1,46 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { useHistory } from 'react-router'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router';
 import {
   Box,
   List,
   ListItem,
   ListItemButton,
-  ListItemText
-} from '@mui/material'
-import { FixedSizeList } from 'react-window'
-import { DataGrid } from '@mui/x-data-grid'
-import api from '../../api'
-import NavBar from '../NavBar'
-import Loader from '../Loader'
+  ListItemText,
+} from '@mui/material';
+import { FixedSizeList } from 'react-window';
+import { DataGrid } from '@mui/x-data-grid';
+import api from '../../api';
+import NavBar from '../NavBar';
+import Loader from '../Loader';
 
 const columns = [
   // { field: 'id', headerName: 'id', width: 100 },
   // { field: 'sharing_id', headerName: 'id', width: 100 },
   // { field: 'portfolio_id', headerName: 'Portfolio id', width: 150 },
   { field: 'portfolio_name', headerName: 'Portfolio', width: 150 },
-  { field: 'owner', headerName: 'Owner', width: 130 }
-]
+  { field: 'owner', headerName: 'Owner', width: 130 },
+];
 
 const columns2 = [
   // { field: 'id', headerName: 'id', width: 100 },
   // { field: 'sharing_id', headerName: 'id', width: 100 },
   // { field: 'portfolio_id', headerName: 'Portfolio id', width: 150 },
   { field: 'portfolio_name', headerName: 'Portfolio', width: 150 },
-  { field: 'shared_with', headerName: 'Shared_with', width: 130 }
-]
+  { field: 'shared_with', headerName: 'Shared_with', width: 130 },
+];
 
-function CollabList () {
-  var history = useHistory()
-  const [title, setTitle] = React.useState('')
-  const [sharingMeLen, setSharingMeLen] = React.useState(false)
-  const [sharedThemLen, setSharedThemLen] = React.useState(0)
-  const [collabPortMe, setCollabPortMe] = useState([])
-  const [collabPortThem, setCollabPortThem] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+function CollabList() {
+  var history = useHistory();
+  const [title, setTitle] = React.useState('');
+  const [sharingMeLen, setSharingMeLen] = React.useState(false);
+  const [sharedThemLen, setSharedThemLen] = React.useState(0);
+  const [collabPortMe, setCollabPortMe] = useState([]);
+  const [collabPortThem, setCollabPortThem] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(async () => {
-    setIsLoading(true)
+    setIsLoading(true);
 
     // // psedo data
     // const me = [
@@ -60,21 +60,20 @@ function CollabList () {
     await api(
       `collaborate/shared-with-me?token=${sessionStorage.getItem('token')}`,
       'GET'
-    )
-    .then((dataMe) => {
+    ).then((dataMe) => {
       // // print out result
       // dataMe.map(d => {
       //   console.log('m', d);
       // })
 
-      setSharingMeLen(dataMe.length)
+      setSharingMeLen(dataMe.length);
       setCollabPortMe(
-        dataMe.map(d => {
-          d.id = d.sharing_id
-          return d
+        dataMe.map((d) => {
+          d.id = d.sharing_id;
+          return d;
         })
-      )
-    })
+      );
+    });
 
     // // psedo data
     // const them = [
@@ -100,10 +99,11 @@ function CollabList () {
     // })
 
     await api(
-      `collaborate/sharing-with-others?token=${sessionStorage.getItem('token')}`,
+      `collaborate/sharing-with-others?token=${sessionStorage.getItem(
+        'token'
+      )}`,
       'GET'
-    )
-    .then((dataThem) => {
+    ).then((dataThem) => {
       let isShared = false;
       // // print out result
       // console.log('them', dataThem)
@@ -114,84 +114,89 @@ function CollabList () {
       //   }
       // })
 
-      if (isShared){
-        setSharedThemLen(dataThem.length)
+      if (isShared) {
+        setSharedThemLen(dataThem.length);
         setCollabPortThem(
-          dataThem.map(d => {
-            d.id = d.portfolio_id
-            return d
+          dataThem.map((d) => {
+            d.id = d.portfolio_id;
+            return d;
           })
-        )
+        );
       }
-    })
+    });
 
-    setIsLoading(false)
-  }, [])
+    setIsLoading(false);
+  }, []);
+
+  const onRowClick = (item) => {
+    // console.log('selected id', item);
+    // console.log('collabPortMe', collabPortMe);
+    history.push(`portfolio/${item.id}`);
+  };
 
   return (
     <div>
-      <hr/>
+      <hr />
       <div>
         <h3>Collaboration List</h3>
         <br />
         {isLoading && <Loader />}
         <div style={{ height: 400, width: '100%' }}>
-          {!isLoading &&
-            sharingMeLen != 0 && <p>Portfolio That Sharing Permission To Me</p>}
-          {!isLoading &&
-            sharingMeLen != 0 && (
-              <DataGrid
-                rows={collabPortMe}
-                columns={columns}
-                pagination
-                checkboxSelection
-                pageSize={7}
-                rowCount={100}
-                paginationMode="server"
-              />
-            )}
-          {!isLoading &&
-            sharingMeLen == 0 && (
-              <div>
-                <p>No portfolio share with you yet.</p>
-              </div>
-            )}
-          {!isLoading &&
-            sharedThemLen != 0 && <p>Portfolio That I Shared With Others</p>}
-          {!isLoading &&
-            sharedThemLen != 0 && (
-              // <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-              //   {collabPortThem.map((them) => {
-              //     <ListItem>
-              //       <ListItemText primary={`Portfolio Name ${them.portfolio_name}`} secondary={
-              //         <div>
-              //           {them.shared_with.map((shared) => {
-              //             <div>{shared}</div>
-              //           })}
-              //         </div>
-              //       }
-              //       />
-              //     </ListItem>
-              //   })}
-              // </List>
-              <DataGrid
-                rows={collabPortThem}
-                columns={columns2}
-                pagination
-                checkboxSelection
-                pageSize={7}
-                rowCount={100}
-                paginationMode="server"
-              />
-            )}
-          {!isLoading &&
-            sharedThemLen == 0 && (
-              <p>You haven't shared portfolio with anyone yet.</p>
-            )}
+          {!isLoading && sharingMeLen != 0 && (
+            <p>Portfolio That Sharing Permission To Me</p>
+          )}
+          {!isLoading && sharingMeLen != 0 && (
+            <DataGrid
+              rows={collabPortMe}
+              columns={columns}
+              pagination
+              checkboxSelection
+              pageSize={7}
+              rowCount={100}
+              paginationMode="server"
+              onRowClick={(item) => onRowClick(item)}
+            />
+          )}
+          {!isLoading && sharingMeLen == 0 && (
+            <div>
+              <p>No portfolio share with you yet.</p>
+            </div>
+          )}
+          {!isLoading && sharedThemLen != 0 && (
+            <p>Portfolio That I Shared With Others</p>
+          )}
+          {!isLoading && sharedThemLen != 0 && (
+            // <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            //   {collabPortThem.map((them) => {
+            //     <ListItem>
+            //       <ListItemText primary={`Portfolio Name ${them.portfolio_name}`} secondary={
+            //         <div>
+            //           {them.shared_with.map((shared) => {
+            //             <div>{shared}</div>
+            //           })}
+            //         </div>
+            //       }
+            //       />
+            //     </ListItem>
+            //   })}
+            // </List>
+            <DataGrid
+              rows={collabPortThem}
+              columns={columns2}
+              pagination
+              checkboxSelection
+              pageSize={7}
+              rowCount={100}
+              paginationMode="server"
+            />
+          )}
+          {!isLoading && sharedThemLen == 0 && (
+            <p>You haven't shared portfolio with anyone yet.</p>
+          )}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default CollabList
+export default CollabList;
