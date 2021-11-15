e601bf1cd3c62dfb606860360023c92d3d408eab
Tony Jang <eui.chang@student.unsw.edu.au>
2021-11-15 19:57:44 +1100

    some stylings

--- stock-portfolio/src/components/TaxStatement/index.js
+++ stock-portfolio/src/components/TaxStatement/index.js
@@ -1,50 +1,48 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import api from '../../api'
-import { Box, TextField } from '@mui/material'
-import Loader from '../Loader'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import api from '../../api';
+import { Box, TextField } from '@mui/material';
+import Loader from '../Loader';
 
-function TaxStatement () {
-  const [isLoading, setIsLoading] = useState(false)
-  const [prevI, setPrevI] = useState(0)
-  const [income, setIncome] = useState(0)
-  const [rate, setRate] = useState(0)
-  const [tax, setTaxValue] = useState(0)
-  const [editing, setEditing] = useState(false)
+function TaxStatement() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [prevI, setPrevI] = useState(0);
+  const [income, setIncome] = useState(0);
+  const [rate, setRate] = useState(0);
+  const [tax, setTaxValue] = useState(0);
+  const [editing, setEditing] = useState(false);
 
   useEffect(() => {
-    if (sessionStorage.getItem('token') == null) return alert("Not loading the portfolio");
+    if (sessionStorage.getItem('token') == null)
+      return alert('Not loading the portfolio');
 
-    setIsLoading(true)
+    setIsLoading(true);
     api(
-      `invested_performance/tax?token=${localStorage.getItem('token')}`,
+      `invested_performance/tax?token=${sessionStorage.getItem('token')}`,
       'GET'
-    ).then(res => {
+    ).then((res) => {
       if (res.CGT) {
-        setIncome(res.yearly_gain)
-        setTaxValue(res.CGT)
-        setRate(res.to_declare)
+        setIncome(res.yearly_gain);
+        setTaxValue(res.CGT);
+        setRate(res.to_declare);
       } else {
-        alert('No Stocks yet')
+        alert('No Stocks yet');
       }
-    })
-    setIsLoading(false)
-  }, [])
-
+    });
+    setIsLoading(false);
+  }, []);
 
   return (
     <div class="text-center w-100 p-3">
       <form>
-        <h1>
-          Financial Information
-        </h1>
+        <h1>Financial Information</h1>
       </form>
 
       <div>
         <Box
           component="form"
           sx={{
-            '& .MuiTextField-root': { m: 1, width: '25ch' }
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
           }}
           autoComplete="off"
         >
@@ -71,7 +69,7 @@ function TaxStatement () {
         </Box>
       </div>
     </div>
-  )
+  );
 }
 
-export default TaxStatement
+export default TaxStatement;
